820ec965699ccf0c63d993cebf2deaecc7b3ed0e
tfcx-th <1438994285@qq.com>
2019-03-22 21:51:22 +0800

    添加种类

--- SimpleShoppingCart/main.js
+++ SimpleShoppingCart/main.js
@@ -3,77 +3,109 @@ var app = new Vue({
   data: {
     allChecked: false,
     title: '全选',
-    list: [
-      {
-        id: 1,
-        checked: false,
-        name: 'iPhone XR',
-        price: 6499,
-        count: 1
-      },
-      {
-        id: 2,
-        checked: false,
-        name: 'iPad Air',
-        price: 3999,
-        count: 1
-      },
-      {
-        id: 3,
-        checked: false,
-        name: 'Mackbook Pro',
-        price: 15788,
-        count: 1
-      }
-    ]
+    list: 
+      [
+        [
+          {
+            id: '电子产品',
+            checked: false,
+            name: 'iPhone XR',
+            price: 6499,
+            count: 1
+          },
+          {
+            id: '电子产品',
+            checked: false,
+            name: 'iPad Air',
+            price: 3999,
+            count: 1
+          },
+          {
+            id: '电子产品',
+            checked: false,
+            name: 'Mackbook Pro',
+            price: 15788,
+            count: 1
+          }
+        ],
+        [
+          {
+            id: '水果',
+            checked: false,
+            name: 'apple',
+            price: 5,
+            count: 1
+          },
+          {
+            id: '水果',
+            checked: false,
+            name: 'banana',
+            price: 3,
+            count: 1
+          },
+          {
+            id: '水果',
+            checked: false,
+            name: 'orange',
+            price: 15,
+            count: 1
+          }
+        ]
+      ]
   },
 
   computed: {
     totalPrice: function() {
       var total = 0;
       for (var i = 0; i < this.list.length; i++) {
-        var item = this.list[i];
-        if (item.checked === true) {
-          total += item.price * item.count;
-        } 
+        for (var j = 0; j < this.list[i].length; j++) {
+          var item = this.list[i][j];
+          if (item.checked === true) {
+            total += item.price * item.count;
+          } 
+        }
       }
       return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
     }
   },
 
   methods: {
-    handleReduce: function(index) {
-      if (this.list[index].count === 1)
+    handleReduce: function(index, _index) {
+      if (this.list[index][_index].count === 1)
         return;
-      this.list[index].count--;
+      this.list[index][_index].count--;
     },
 
-    handleAdd: function(index) {
-      this.list[index].count++;
+    handleAdd: function(index, _index) {
+      this.list[index][_index].count++;
     },
 
-    handleRemove: function(index) {
-      this.list.splice(index, 1);
+    handleRemove: function(index, _index) {
+      this.list[index].splice(_index, 1);
     },
 
-    handleCheckbox: function(index) {
-      if (this.list[index].checked === false) {
-        this.list[index].checked = true;
+    handleCheckbox: function(index, _index) {
+      if (this.list[index][_index].checked === false) {
+        this.list[index][_index].checked = true;
       } else {
-        this.list[index].checked = false;
+        this.list[index][_index].checked = false;
       }
     },
 
     handleCheckAll: function() {
       if (!this.allChecked) {
         for (var i = 0; i < this.list.length; i++) {
-          this.list[i].checked = true;
+          for (var j = 0; j < this.list[i].length; j++) {
+            this.list[i][j].checked = true;
+          }
         }
         this.allChecked = true;
         this.title = '全不选';
       } else {
         for (var i = 0; i < this.list.length; i++) {
-          this.list[i].checked = false;
+          for (var j = 0; j < this.list[i].length; j++) {
+            this.list[i][j].checked = false;
+          }
         }
         this.allChecked = false;
         this.title = '全选';
